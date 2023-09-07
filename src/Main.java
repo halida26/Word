67c5624e21bdbb29be9646a61aa35d5b8400835e
halida26 <sydygalievahalida2@gmail.com>
2023-09-07 10:48:28 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+public class Main {
+    public static void main(String[] args) {
+        int number=4;
+       double num=5.7;
+       float numb=4;
+       boolean b = true;
+       char sim ='a';
+       String str ="ayana";
+        System.out.println();
+        if(number!=5){
+            
+        }
+        else {
+            System.out.println("Emilbek");
+        }
+        System.out.println(Ayana2());
+        Ayana(5);
+    }
+    public static void Ayana(int number){
+        System.out.println(number);
+    }
+    public static void Ayana1(){
+       int number=5;
+        System.out.println(number);
+        System.out.println(number);
+    }
+    public static char Ayana2(){
+        return '5';
+    }
+
+}
